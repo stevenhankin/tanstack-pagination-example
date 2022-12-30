3d3308c399df8c5f338cba3f554dad097e0aef89
Steven Hankin <steven.hankin@gmail.com>
2022-12-30 13:05:05 +0000

    Extract PagedCharacters component

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,30 @@ import { useState } from "react";
 import { QueryKey, useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { CharacterResponse, getData } from "./api";
+import PagedCharacters from "./PagedCharacters";
 
 function App() {
   const [page, setPage] = useState(1);
-
-  const queryKey: QueryKey = ["characters", page];
-
-  const { status, error, data, isPreviousData } = useQuery<
-    CharacterResponse,
-    AxiosError
-  >({
-    queryKey,
-    queryFn: () => getData({ queryKey }),
-  });
+  const [hasNext, setHasNext] = useState<boolean>();
 
   return (
     <div>
       <div>
         <button
-          disabled={isPreviousData || page <= 1}
-          onClick={() => setPage((p) => p - 1)}
+          disabled={page <= 1}
+          onClick={() => setPage((prev) => prev - 1)}
         >
           Previous
         </button>
+
         <span>Page: {page}</span>
-        <button
-          disabled={isPreviousData || !data || !data.next}
-          onClick={() => setPage((old) => old + 1)}
-        >
+
+        <button disabled={!hasNext} onClick={() => setPage((prev) => prev + 1)}>
           Next
         </button>
       </div>
-      {status === "loading" ? (
-        <div>Loading...</div>
-      ) : data ? (
-        status === "error" ? (
-          <div>{error!.message}</div>
-        ) : (
-          <div>
-            {data.results.map((d) => (
-              <div key={d.name}>{d.name}</div>
-            ))}
-          </div>
-        )
-      ) : null}
+
+      <PagedCharacters page={page} setHasNext={setHasNext} />
     </div>
   );
 }
---
+++ src/PagedCharacters.tsx
@@ -0,0 +1,49 @@
+import { QueryKey, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useEffect } from "react";
+import { CharacterResponse, getData } from "./api";
+
+type PropsPagedCharacters = {
+  page: number;
+  setHasNext: React.Dispatch<React.SetStateAction<boolean | undefined>>;
+};
+
+/**
+ * Example of paginating Characters
+ */
+const PagedCharacters: React.FC<PropsPagedCharacters> = ({
+  page,
+  setHasNext,
+}) => {
+  const queryKey: QueryKey = ["characters", page];
+
+  const { status, error, data } = useQuery<CharacterResponse, AxiosError>({
+    queryKey,
+    queryFn: () => getData({ queryKey }),
+  });
+
+  /**
+   * State (that tracks whether there is a "next" page) is lifted
+   */
+  useEffect(() => {
+    setHasNext(data?.next ? true : false);
+  }, [data]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "error") {
+    return <div>{error!.message}</div>;
+  }
+
+  return (
+    <div>
+      {data.results.map((d) => (
+        <div key={d.name}>{d.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default PagedCharacters;
